c0fa2d03e2c2b80f7a564173622e191e3fad0b47
JuneJoshua <metroidprimeshampoo@gmail.com>
2017-06-05 20:20:04 -0700

    Add files via upload

---
+++ characters.c
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+#include <stdio.h>
+int main()
+{
+    printf("%s is the best teammate ever!\n", "Samus Aran");
+    printf("I have played 'Nobody' at least %i times\n", 400);
+    printf("Pi equals %f\n", 3.14159);
+    return 0;
+}
---
+++ print.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    /*
+        Print on screen
+    */
+    // Metroid: Other M
+    printf("Perfect! Congratulations!\n");
+
+    // Rough (G-Friend)
+    printf("If time goes by and I become an adult\n");
+    printf("Amidst the divergence, I will hold your hand\n");
+
+}
