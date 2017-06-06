49424958aca83b038ea69bcac80e8b32b4e8e439
JuneJoshua <metroidprimeshampoo@gmail.com>
2017-06-06 12:13:14 -0700

    Add files via upload

---
+++ password.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    if(10 == 110){
+        printf("Correct!\n");
+    }
+    else{
+        printf("Your password is incorrect!\n");
+    }
+
+    return 0;
+}
