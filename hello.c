32188151a2b2bd3278e7e68ef20752f4fbb8b3be
JuneJoshua <metroidprimeshampoo@gmail.com>
2017-06-05 20:23:51 -0700

    Update and rename print.c to hello.c

--- hello.c
+++ hello.c
@@ -1,14 +1,14 @@
+/*
+Prints messages on screen
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 int main()
 {
-    /*
-        Print on screen
-    */
-    // Metroid: Other M
+    
     printf("Perfect! Congratulations!\n");
-
-    // Rough (G-Friend)
+    print("Hello, World!\n");
     printf("If time goes by and I become an adult\n");
     printf("Amidst the divergence, I will hold your hand\n");
 
