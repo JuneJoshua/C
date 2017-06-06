c329d3dc087253d65a8d64a54253c543f8c97fe1
JuneJoshua <metroidprimeshampoo@gmail.com>
2017-06-05 20:20:59 -0700

    Update characters.c

--- characters.c
+++ characters.c
@@ -2,8 +2,13 @@
 #include <stdio.h>
 int main()
 {
+    //String
     printf("%s is the best teammate ever!\n", "Samus Aran");
+    
+    //Integer
     printf("I have played 'Nobody' at least %i times\n", 400);
+    
+    //Float
     printf("Pi equals %f\n", 3.14159);
     return 0;
 }
