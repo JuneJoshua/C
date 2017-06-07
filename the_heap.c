9254f5ada7f76efbde02a46a791de99d291619af
JuneJoshua <metroidprimeshampoo@gmail.com>
2017-06-07 14:20:36 -0700

    Add files via upload

---
+++ the_heap.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include <math.h>
+int main()
+{
+    int * points;
+    points = (int *) malloc(5 * sizeof(int));
+    free(points);
+    return 0;
+}
