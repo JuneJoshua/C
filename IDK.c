5508cd225540f1db2f2dee9a39cb924a63c7d624
JuneJoshua <metroidprimeshampoo@gmail.com>
2017-06-06 19:37:55 -0700

    Add files via upload

---
+++ IDK.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include <math.h>
+int main()
+{
+    int shampoo;
+    for(shampoo = 1; shampoo <= 5; shampoo++){
+        printf("I get nervous everytime I think about you.\n");
+    }
+}
