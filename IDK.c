847c6f595befb9ffba706f5149bd277c2a17310a
JuneJoshua <metroidprimeshampoo@gmail.com>
2017-06-06 19:39:56 -0700

    Update IDK.c

--- IDK.c
+++ IDK.c
@@ -7,6 +7,8 @@ int main()
 {
     int shampoo;
     for(shampoo = 1; shampoo <= 5; shampoo++){
+        // This is a song lyrics from the song I don't know(IDK)
         printf("I get nervous everytime I think about you.\n");
     }
+    return 0;
 }
