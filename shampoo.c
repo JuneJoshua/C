ffd17267c3263bfca8d67828778303b650dfb763
JuneJoshua <metroidprimeshampoo@gmail.com>
2017-06-06 19:29:07 -0700

    Add files via upload

---
+++ shampoo.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int i = 1;
+    while(i < 10){
+        printf("Number of times played as Zero Suit Samus: %d\n", i);
+        i++;
+    }
+
+    int j = 13;
+    do{
+        printf("I want to be the shampoo on your hair\n");
+    }while(j < 12 && j > 20);
+    return 0;
+}
