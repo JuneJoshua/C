e190faf0d2f768e6f4531419ff4e62ed97016e1e
JuneJoshua <metroidprimeshampoo@gmail.com>
2017-06-07 18:22:02 -0700

    Add files via upload

---
+++ global_variables.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+void printSomething();
+
+int main()
+{
+    int wart = 11;
+    printf("I have beaten Wart %d times\n", wart);
+    return 0;
+}
+
+void printSomething(){
+    printf("I have beaten Wart %d times\n", wart);
+    return;
+}
