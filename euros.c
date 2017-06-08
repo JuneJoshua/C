383ffcff14c0f4445221dc93871bbc52c020cf65
JuneJoshua <metroidprimeshampoo@gmail.com>
2017-06-07 18:43:45 -0700

    Add files via upload

---
+++ euros.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void convertToDollars(float euro);
+
+int main(){
+    float euroPrice1 = 1.00;
+    convertToDollars(euroPrice1);
+    return 0;
+}
+void convertToDollars(float euro){
+    float usd = euro * 1.37;
+    printf("%.2f Euros - %.2f USD.\n", euro, usd);
+    return;
+
+}
